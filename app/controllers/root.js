8ab4d0b633899943f33287b86ef4727b641b816d
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-04-17 20:36:40 +0700

    create root controller

---
+++ app/controllers/root.js
@@ -0,0 +1,34 @@
+const db = require('../models/database');
+
+const index = async (req, res) => {
+  const todos = await db.table('todos').all();
+  res.render('index.ejs', { todos: todos.rows });
+};
+
+const add = (req, res) => {
+  res.render('add.ejs');
+};
+
+const save = async (req, res) => {
+  const todo = req.body
+    .split('&')
+    .map(t => unescape(t.replace('+', ' ')))
+    .map(t => t.split('='))
+    .reduce((acc, curr) => (acc[curr[0]] = curr[1], acc), {});
+
+  const insert = await db.table('todos').insert(todo);
+
+  // will update later
+  // res.redirect('/');
+  res.writeHead(302, {
+    'Location': '/'
+  });
+  res.end();
+};
+
+module.exports = {
+  index,
+  add,
+  save,
+};
+
